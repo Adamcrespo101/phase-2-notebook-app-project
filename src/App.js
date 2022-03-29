4bda5d2f1194daff65a0d5e11ce7e3822bb32cf2
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-03-29 16:50:45 -0400

    me like to draw. me think it fun

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import './App.css';
 import NotesList from './Components/NotesList';
 import Searchbar from "./Components/Searchbar";
+import DrawingArea from './Components/DrawingArea';
 import { useState, useEffect } from 'react'
 
 function App() {
-  //deliverables for march 29th - click to edit each note, more styling 
+  //deliverables march 30th dynamic timestamps for posts, add animations/styling for listener events
   const [notes, setNotes] = useState([])
   const [search, setSearch] = useState('')
+  const [cat, setCat] = useState({category: 'All'})
+  const [mode, setMode] = useState(false)
 
+  
   useEffect(() => {
     fetch('http://localhost:3001/notes')
     .then(res => res.json())
     .then(data => setNotes(data))
   }, [])
-
+  
+  function handleCatFetch(){
+    if (cat.category === "All"){
+      fetch('http://localhost:3001/notes')
+      .then(res => res.json())
+      .then(data => setNotes(data))
+    } else {
+      fetch(`http://localhost:3001/notes?category=${cat.category}`)
+      .then(res => res.json())
+      .then(data => setNotes(data))
+    }
+  }
   const searchedNotes = notes.filter(note => note.body.toLowerCase().includes(search))
+
+  function handleMode(){
+    setMode(prev => !prev)
+  }
   
   return (
     <div className="App">
-      <Searchbar search={search} setSearch={setSearch} />
-      <NotesList notes={notes} setNotes={setNotes} searchedNotes={searchedNotes} />
+      <button onClick={handleMode}>Toggle Note pad/Drawing pad</button>
+      {
+        mode ? <>
+                  <DrawingArea />
+                </> 
+              :
+        <>
+          <Searchbar search={search} setSearch={setSearch} setCat={setCat} handleCatFetch={handleCatFetch} />
+          <NotesList notes={notes} setNotes={setNotes} searchedNotes={searchedNotes} />
+        </>
+      }
     </div>
   );
 }
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,6 +1,6 @@
 
 
-function Searchbar({search, setSearch}){
+function Searchbar({search, setSearch, handleCatFetch, setCat}){
 
         function handleSearch(e){
             setSearch(e.target.value)
@@ -9,18 +9,19 @@ function Searchbar({search, setSearch}){
 
     return(
         <div className="search-container">
-            <h1 id="more-notes">More-Notes</h1>
+            <h1 className="more-notes">More-Notes</h1>
         <input type="text" placeholder="search for a note here..." onChange={handleSearch}/>
         <input type="submit" value="🔍"/>
         <div className="search-by-cat">
         <label>Search By Category:</label>
-        <select className="category-select">
+        <select className="category-select" onChange={(e) => setCat({category: e.target.value})}>
             <option name="all">All</option>
             <option name="reminder">Reminder</option>
             <option name="personal">Personal</option>
             <option name="errand">Errand</option>
             <option name="misc">Misc.</option>
         </select>
+        <button onClick={handleCatFetch} id="filter-cat">🔍</button>
         </div>
         </div>
     )
